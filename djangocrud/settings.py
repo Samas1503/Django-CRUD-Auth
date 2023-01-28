30646cda3d229d9f72382fe540fc68fd5db03aa7
Samas1503 <samueleliasparedes.10@gmail.com>
2023-01-28 01:26:58 -0300

    third commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,12 +83,10 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default' : dj_database_url.config(
-            default='postgresql:///postgres:postgres@localhost/postgres',
-            conn_max_age=600
-        )
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
