7e9f17c9927752b2be4f8a2b58f28fcd120c1135
Samas1503 <samueleliasparedes.10@gmail.com>
2023-01-28 13:20:28 -0300

    fiveth commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 # This setting tells Django at which URL static files are going to be served to the user.
 # Here, they well be accessible at your-domain.onrender.com/static/...
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+if 'RENDER' in os.environ:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
